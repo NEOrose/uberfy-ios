5d2edcabdc33635eca0723133a98d019544f10f7
haroldoolivieri <olivierisoares@gmail.com>
2016-06-02 15:27:25 -0300

    Initial Commit

---
+++ UberfyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UberfyApp
+//
+//  Created by Haroldo Olivieri on 6/2/16.
+//  Copyright © 2016 neorose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
